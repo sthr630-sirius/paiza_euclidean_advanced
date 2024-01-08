bdea9ea6e809b201d265d91997bb59a52f096758
sthr630-arcturus <s_izumi630y@yahoo.co.jp>
2024-01-08 15:34:31 +0900

    edit a_step7

--- a_step7.py
+++ a_step7.py
@@ -7,10 +7,6 @@ def gcd(a, b):
 def lcm(a, b):
     return a*b//gcd(a,b)
 
-n = int(input())
-l = int(input())-1
-for  _ in range(n-1):
-    a = int(input())-1
-    l = lcm(l,  a)
-
-print(l)
+a, b = map(int, input().split())
+c = lcm(a, b)
+print(c)
